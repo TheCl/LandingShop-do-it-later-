31ed94bcae1587a82a179ef30a310ae67e9ee3e8
TheCl <sfpwolf@mail.ru>
2017-07-14 02:09:50 +0300

    Pre-last commit

--- js/common.js
+++ js/common.js
@@ -9,6 +9,13 @@ $(document).ready(function(){
 
 	});
 
+	$(".mobile__nav").click(function() {
+
+		$(this).toggleClass('open');
+		$("nav").slideToggle(1);
+
+	});
+
 	     $(".scroll").click(function () { 
       var elementClick = $(this).attr("href");
       var destination = $(elementClick).offset().top;
@@ -18,6 +25,7 @@ $(document).ready(function(){
 
 
 	$('.slick').slick({
+	  arrows: true,
 	  speed: 500,
 	  slidesToShow: 5,
 	  slidesToScroll: 1,
@@ -56,11 +64,12 @@ $(document).ready(function(){
 	  slidesToShow: 3,
 	  slidesToScroll: 1,
 	  infinite: false,
+	  nextArrow: $(".GOD"),
+      prevArrow: $(".PLS"),
 	  responsive: [
 	    {
 	      breakpoint: 1200,
 	      settings: {
-	        arrows: false,
 	        slidesToShow: 2,
 	        slidesToScroll: 1,
 	      }
@@ -68,13 +77,18 @@ $(document).ready(function(){
 	    {
 	      breakpoint: 768,
 	      settings: {
-	        arrows: false,
 	        slidesToShow: 1,
 	        slidesToScroll: 1,
 	      }
 	    },
 	  ]
 	});
+
+		$('.slick_3').slick({
+
+	  nextArrow: $(".next"),
+      prevArrow: $(".prev"),
+		});
 
 
 });
